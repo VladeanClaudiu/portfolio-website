998cc3972eaff7e58867f1daa67b9418cf6dff3d
ClaudiuVladean <claudiuve3.2@gmail.com>
2022-09-09 15:31:01 +0100

    css changes

--- src/components/Test.jsx
+++ src/components/Test.jsx
@@ -1,6 +1,9 @@
 export default function Test() {
     return (
         <div>
+            <header>
+                <span>CV</span>
+            </header>
             <h1>this is a heading 1</h1>
             <h2>this is a heading 2</h2>
             <h3>this is a heading 3</h3>
