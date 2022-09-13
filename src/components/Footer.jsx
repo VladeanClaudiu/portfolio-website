20bbb6b47c68161ae05cfd1df9402482d5b9fefd
ClaudiuVladean <claudiuver3.2@gmail.com>
2022-09-13 13:12:57 +0100

    added blank targets so that new tabs open when links are clicked

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,8 +4,8 @@ export default function Footer() {
         <footer className="footer-section">
             <div className="footer-div"></div>
             <h4>more of me here!</h4>
-            <p>Github: <span className="span-underline"><a href="https://github.com/VladeanClaudiu">VladeanClaudiu</a></span></p>
-            <p>LinkedIn: <span className="span-underline"><a href="https://www.linkedin.com/in/claudiu-vladean-652614191/">Claudiu Vladean</a></span></p>
+            <p>Github: <span className="span-underline"><a target="_blank" href="https://github.com/VladeanClaudiu">VladeanClaudiu</a></span></p>
+            <p>LinkedIn: <span className="span-underline"><a target="_blank" href="https://www.linkedin.com/in/claudiu-vladean-652614191/">Claudiu Vladean</a></span></p>
             
         </footer>
     )
