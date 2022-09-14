ae6b6ff8603ec68bc787ea73028e800ab45d6414
ClaudiuVladean <claudiuver3.2@gmail.com>
2022-09-14 09:48:11 +0100

    created new router

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,16 @@
 import './App.css'
 import About from './components/About'
-import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 
 function App() {
-  const [darkMode, setDarkMode] = useState(true);
-
-  function changeMode() {
-    console.log("clicked")
-    console.log(darkMode)
-    setDarkMode(!darkMode);
-  }
   return (
-    <section className={darkMode? "appSectionLight" : "app-section"}>
-      <About
-      darkMode = {darkMode}
-      handleClick = {changeMode}/>
-    </section>
+    <Router>
+      <Routes>
+        <Route path="/" element={<About />}/>
+        <Route path="/projects" element={<About />} />
+        <Route path="/projects" element={<About />} />
+      </Routes>
+    </Router>
   )
 }
 
