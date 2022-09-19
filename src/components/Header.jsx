d28ea590b3bdb7af607cd24e60a0a41ec61b8ecb
VladeanClaudiu <claudiuver3.2@gmail.com>
2022-09-19 15:30:52 +0100

    css changes to projects page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ export default function Header(props) {
     return (
         <header className="header-section">
                 <span className={`initial-logo ${props.darkMode? "initialLogo-light" : ""}`}>CV</span>
-                <h2>{props.heading}</h2>
+                <h2 className="header-title">{props.heading}</h2>
         </header>
     )
 }
