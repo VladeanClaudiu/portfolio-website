0d1babdb4c672ea85a4d65a9a72b296b672e06dd
VladeanClaudiu <claudiuver3.2@gmail.com>
2022-09-21 10:00:50 +0100

    css changes to darkmode form

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -7,7 +7,7 @@ export default function ContactForm(props) {
             <h2 className={`heading2 ${props.darkMode ? "heading2-light" : "heading2-dark"}`}>Say Hi!</h2>
         	
             <p>
-                Dont hesitate to message me, I can be reached very easily. Fill in the from below to email at <span className={`${props.darkMode ? "span-underline-lightMode underlineLightmode" : "span-underline underlineDarkmode"}`}>claudiuver1.0@gmail.com </span>. 
+                Dont hesitate to message me, I can be reached very easily. Fill in the from below to email me at <span className={`${props.darkMode ? "span-underline-lightMode underlineLightmode" : "span-underline underlineDarkmode"}`}>claudiuver1.0@gmail.com </span>. 
             </p>
 
             <form className={`contact-form ${props.darkMode ? "form-light" : "form-dark"}`} onSubmit={handleSubmit}>
@@ -25,6 +25,7 @@ export default function ContactForm(props) {
                 </div>
                 <button className={`contct-btn ${props.darkMode ? "btn-dark" : "btn-light"}`} type="submit">Send Message</button>
             </form>
+            <p className="form-paragraph">I am currently looking for a change and recently started applying for Frontend/React developer positions. If I'm a good fit for a role please contact me.</p>
         </section>
        
     )
