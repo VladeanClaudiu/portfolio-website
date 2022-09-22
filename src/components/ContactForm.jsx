b9488dddd888d93f1748c0b83b11771046f9d87b
VladeanClaudiu <claudiuver3.2@gmail.com>
2022-09-22 09:20:59 +0100

    handle submit function modified for submission

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,5 +1,26 @@
+import { useState } from "react"
+
 export default function ContactForm(props) {
-    function handleSubmit(){
+    const [status, setStatus] = useState("Submit");
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        setStatus("Sending...");
+        const { name, email, message} = e.target.elements;
+
+        let details = {
+            name: name.value,
+            email: email.value,
+            message: message.value,
+        };
+
+        let resonse = await fetch("http://localhost:5173//contact", {
+            method: "POST",
+            headers : {
+                "Content-Type": "application/json;charset=utf-8",
+            },
+            body: JSON.stringify(details),
+        })
         console.log("Submitted")
     }
     return (
